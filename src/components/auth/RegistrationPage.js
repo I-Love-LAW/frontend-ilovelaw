4c9df90b19538d54f4b7be9790df96faa3b8e5fb
Ramdhan Firdaus Amelia <ramdhan.firdaus@ui.ac.id>
2023-05-11 11:37:14 +0700

    Mengubah ketentuan minimum dan maksimum

--- src/components/auth/RegistrationPage.js
+++ src/components/auth/RegistrationPage.js
@@ -21,11 +21,11 @@ const registrationSchema = Yup.object().shape({
         .required('Name is required'),
     username: Yup.string()
         .min(3, 'Minimum 3 symbols')
-        .max(50, 'Maximum 50 symbols')
+        .max(50, 'Maximum 20 symbols')
         .required('Username is required'),
     password: Yup.string()
         .min(6, 'Minimum 6 symbols')
-        .max(50, 'Maximum 50 symbols')
+        .max(50, 'Maximum 40 symbols')
         .required('Password is required'),
     changepassword: Yup.string()
         .required('Password confirmation is required')
