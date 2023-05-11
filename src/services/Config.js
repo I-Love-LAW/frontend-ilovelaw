aee6fa479c23e9b4b7a104ba8df9c4f7651e2a2b
Ramdhan Firdaus Amelia <ramdhan.firdaus@ui.ac.id>
2023-05-11 17:42:10 +0700

    Change auth port

--- src/services/Config.js
+++ src/services/Config.js
@@ -1,5 +1,7 @@
-export function AUTH_BACKEND_URL (url) {
-    return (
-        "http://localhost:8080/" + url
-    )
+export function AUTH_BACKEND_URL(url) {
+  return "http://localhost:8082/" + url;
+}
+
+export function CONVERT_BACKEND_URL(url) {
+  return "http://localhost:8080/" + url;
 }
